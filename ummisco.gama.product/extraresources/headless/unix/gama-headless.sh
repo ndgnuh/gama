611fec63750649506d975afa1d3c9fde147ff5cb
Arthur Brugiere <contact@arthurbrugiere.fr>
2019-07-25 13:53:17 +0700

    [HL-script] update meta data header

--- ummisco.gama.product/extraresources/headless/unix/gama-headless.sh
+++ ummisco.gama.product/extraresources/headless/unix/gama-headless.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 
 ##
-# Copyright 2019 Arthur Brugiere <contact@arthurbrugiere.fr>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-##
-#
-# This script aim to be a helper to use the GAMA's headless
-# 
+#  gama-headless.sh
+#  Author: Arthur Brugiere <RoiArthurB>
+#  Description: This script aim to be a helper to use the GAMA's headless
+#  Last editing: 07/19
 ##
 
 #
